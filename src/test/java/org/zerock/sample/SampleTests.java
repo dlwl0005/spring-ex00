2013b6074d1ef49c4c94874330e23b1dd449f980
이지용 <@121.160.115.61>
2021-01-07 17:10:47 +0900

    d

--- src/test/java/org/zerock/sample/SampleTests.java
+++ src/test/java/org/zerock/sample/SampleTests.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,13 +19,33 @@ public class SampleTests {
 	
 	@Autowired
 	private ApplicationContext context;
+	
+	@Setter(onMethod_ = {@Autowired})
+	private Restaurant restaurant;
+	
+	
+	
+
+	public void testExist() {
+		assertNotNull(restaurant);
+		
+		log.info(restaurant);
+		log.info("-------------");
+		log.info(restaurant.getChef());
+	}
 
 	@Test
-	public void testExist() {
+	public void testExist2() {
+		Chef c1 =(Chef) context.getBean("chef");
+		Restaurant r1 = (Restaurant) context.getBean("restaurant");
+		
+		assertNotNull(r1.getChef());
+		assertEquals(c1,r1.getChef());
 		log.info("------");
 		assertNotNull(context);
 		assertNotNull(context.getBean("chef"));
 		assertNotNull(context.getBean("restaurant"));
+		
 	}
 
 }
